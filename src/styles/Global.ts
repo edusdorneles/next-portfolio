781393ac4d4a2777b22de93605bda364a66410bb
Eduardo Dorneles <eduardo.dsdorneles@gmail.com>
2021-11-20 20:06:04 -0300

    🐛 Fix: Fonte do projeto

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -2,8 +2,6 @@ import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap');
-
     * {
         margin: 0;
         padding: 0;        
