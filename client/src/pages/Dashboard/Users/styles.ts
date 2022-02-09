ccfc8cc2bc2de8f63925eb7473e39dacb1ee48d2
dududornelees <eduardo.dsdorneles@gmail.com>
2022-02-09 11:07:03 -0300

    🔥 New: Botão para adicionar usuário

--- client/src/pages/Dashboard/Users/styles.ts
+++ client/src/pages/Dashboard/Users/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { shade } from "polished";
 
 export const UsersStyle = styled.main`
   .users__container {
@@ -6,6 +7,26 @@ export const UsersStyle = styled.main`
       display: grid;
       grid-template-columns: 1fr 1fr;
       grid-gap: 15px;
+    }
+
+    .user-cards__new-user {
+      border: 2px dashed ${(props) => props.theme.colors.secondary};
+      transition: ${(props) => props.theme.transition};
+      height: 80px;
+      border-radius: 5px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+
+      &:hover {
+        background-color: ${(props) => shade(0.2, props.theme.colors.primary)};
+      }
+
+      svg {
+        color: ${(props) => props.theme.colors.secondary};
+        font-size: 26px;
+      }
     }
   }
 
