ad440c944e7662f1ef15600cd9a3b7407588595e
dududornelees <eduardo.dsdorneles@gmail.com>
2022-08-11 15:13:31 -0300

    🔥 New: API de projetos

---
+++ src/pages/api/projects.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    const projects = [
+        {
+            id: "8d31202b-e582-4ca9-96c5-c47fca4239fa",
+            title: "Aquabit",
+            createdAt: "Dezembro de 2021",
+            image: "https://res.cloudinary.com/dududornelees/image/upload/v1660186429/aquabit_nlpylp.png",
+            description: "Foi passado um design no Figma, desenvolvido utilizando boas práticas e a metodologia mobile-first.",
+            techs: ["React", "TypeScript", "Styled-Components"],
+            github: "https://github.com/AquabitFarm/Aquabit-Landing",
+            website: "https://aquabit.com.br"
+        },
+        {
+            id: "e0f0bfa3-9ad1-4037-8244-99316816d3d8",
+            title: "Micro RVA",
+            createdAt: "Julho de 2022",
+            image: "https://res.cloudinary.com/dududornelees/image/upload/v1660187055/micro-rva_kyshba.png",
+            description: "Projeto criado para eu por eu prática o conceito de micro-frontend, no mesmo projeto, utilizo React, Vue e Angular (RVA).",
+            techs: ["React", "Vue", "Angular", "Single-Spa"],
+            github: "https://github.com/dududornelees/micro-rva",
+        },
+        {
+            id: "2ffc53b3-55fc-40dc-b9c4-8d89cc5cd46c",
+            title: "Redux - To do List",
+            createdAt: "Março de 2022",
+            image: "https://res.cloudinary.com/dududornelees/image/upload/v1660187436/redux-to-do-list_zazcgo.png",
+            description: "Uma lista de tarefas, feita com React, TypeScript e para fazer o gerenciamento global da aplicação, utilize Redux Toolkit.",
+            techs: ["React", "TypeScript", "Redux Toolkit"],
+            github: "https://github.com/dududornelees/redux-to-do-list",
+            website: "https://redux-to-do-list-dududornelees.netlify.app"
+        },
+    ]
+
+    res.status(200).json(projects);
+};
